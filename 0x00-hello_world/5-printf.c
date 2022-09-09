d47f40b09c07db487da81c07eb1b5171789094b4
Balogunmatt <balogunmatthew01@gmail.com>
2022-09-08 17:56:42 -0700

    print with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio>
+
+/**
+ * main - entry point
+ *
+ * Description: this program uses printf instead of puts
+ * this prints out with proper grammar, but the outcome is a piece of art,
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
