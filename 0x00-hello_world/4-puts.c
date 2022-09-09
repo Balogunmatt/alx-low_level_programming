d85e06a3cd1287a875b485c7bcb9f804aa76975f
Balogunmatt <balogunmatthew01@gmail.com>
2022-09-08 17:06:19 -0700

    use puts to print out the string literal

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main: entry point
+ * description: using the main function 
+ * puts: a standard output function to print out a line of string literal
+ * description: to print out "Programming is like building a multilingual puzzle"
+ * return: returns 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
