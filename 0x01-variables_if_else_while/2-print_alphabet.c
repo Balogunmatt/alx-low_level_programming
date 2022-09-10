05e8ae4773cc0205f2593afe44a2f264726796ee
Balogunmatt <balogunmatthew01@gmail.com>
2022-09-10 06:23:26 -0700

    print the alphabet in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: using the main function
+ * This project is about using putchar function
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	return (0);
+}
