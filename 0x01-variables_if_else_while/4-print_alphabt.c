7025e7004c3d9e9a832617674e5e2adfd8f701ec
Balogunmatt <balogunmatthew01@gmail.com>
2022-09-11 13:00:13 -0700

    print out alphabets but omit q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: using the main function and putchar
+ * function to print a set of alphabets
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+			putchar(ch);
+	}
+	return (0);
+}
