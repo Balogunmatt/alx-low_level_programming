32aff86a809abc67b3ceea4e415d25719a9759e3
Balogunmatt <balogunmatthew01@gmail.com>
2022-09-13 15:08:32 -0700

    use print_alphabet function to print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - printing alphabet in lower case
+ *
+ * Description: This function prints all alphabets in lower case
+ * Return: 0 on success
+ */
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+	return;
+}
