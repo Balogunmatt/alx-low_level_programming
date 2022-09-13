d2c1330e9f179452e5b82893e2f89984a6d8e8cb
Balogunmatt <balogunmatthew01@gmail.com>
2022-09-13 16:08:28 -0700

    print alphabets 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,15 +13,14 @@ void print_alphabet_x10(void)
 	int i = 0, j;
 
 	while (i < 10)
+	{
+		j = 'a';
+		while (j <= 'z')
 		{
-			j = 'a';
-
-			while (j <= 'z')
-			{
-				_putchar(j);
-				j++;
-			}
-			_putchar('\n');
-			i++;
+			_putchar(j);
+			j++;
 		}
+		_putchar('\n');
+		i++;
+	}
 }
