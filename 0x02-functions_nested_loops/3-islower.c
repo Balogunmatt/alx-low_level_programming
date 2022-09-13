75a2ca10aee9672c8bd506f447e7475d126833a0
Balogunmatt <balogunmatthew01@gmail.com>
2022-09-13 16:55:21 -0700

    check if an input is lower case letter or not

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lower case letter
+ *
+ * Description: This function check for lower case letter
+ * Return: 1 and 0 on succes
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
