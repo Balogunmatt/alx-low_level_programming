276970bbf69d8449fe869b01e5806a0ca847da63
Balogunmatt <balogunmatthew01@gmail.com>
2022-09-13 18:36:56 -0700

    print the last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,7 @@
 /**
  * print_last_digit - prints the last digit of a number
  * @c: an inputted integer
- * 
+ *
  * Description: this program prints the last digit of a number
  * Return: the last digit on success
  */
