b3708b772def4df67ffacfb11bebe7e96689563e
Balogunmatt <balogunmatthew01@gmail.com>
2022-09-15 01:20:56 -0700

    test the function provided

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * main - entry point
+ *
+ * Description: this project is about testing
+ * the function created in project 0-positive_or_negative.c
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	
+
+	
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,2 @@
+This project is basically on debugging and testing functions
+created in the previous projects.
