eb35ff03705a178b31564881a41d99855ee87c69
Balogunmatt <balogunmatthew01@gmail.com>
2022-09-15 16:01:48 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+ * print_line - print lines
+ * @n: number of time _ should be printed
+ *
+ * Return: no return statement
+ */
+
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+		_putchar(95);
+	_putchar(10);
+}
