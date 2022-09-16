97dea8c8e444f623e0f0921348c6d12e3c26799e
Balogunmatt <balogunmatthew01@gmail.com>
2022-09-16 03:51:39 -0700

    print fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,11 +13,11 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
-			printf("%s ", "Fizz");
+			printf("Fizz ");
 		else if (i % 5 == 0)
-			printf("%s ", "Buzz");
-		else if (i % 3 == 0 && i % 5 == 0)
-			printf("%s ", "FizzBuzz");
+			printf("Buzz ");
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz ");
 		else
 			printf("%d ", i);
 	}
