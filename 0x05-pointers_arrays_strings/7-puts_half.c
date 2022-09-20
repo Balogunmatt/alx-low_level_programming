6d25dea7063bd852b4a3501352b07cf7a4942ec8
Balogunmatt <balogunmatthew01@gmail.com>
2022-09-20 15:45:32 -0700

    print the second part

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,24 +14,23 @@
 void puts_half(char *str)
 {
 	int l = strlen(str);
-	int h = l / 2;
 	int n = (l - 1) / 2;
-	int j = h - 1;
+	int j = n - 1;
 
 	if (l % 2 != 0)
-	{
-		while (*(str + n) != '\0')
-		{
-			_putchar(*(str + n));
-			n++;
-		}
-	}
-	else
 	{
 		while (*(str + j) != '\0')
 		{
 			_putchar(*(str + j));
 			j++;
+		}
+	}
+	else
+	{
+		while (*(str + n) != '\0')
+		{
+			_putchar(*(str + n));
+			n++;
 		}
 	}
 }
