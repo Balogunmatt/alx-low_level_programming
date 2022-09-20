23bac62bf2292579a65a1961a663a37fa1b1406e
Balogunmatt <balogunmatthew01@gmail.com>
2022-09-19 18:31:13 -0700

    print the string in reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+
+/**
+ * print_rev - print string in reverse 
+ * @s: string pointer
+ *
+ * Return: void
+ */
+
+
+void print_rev(char *s)
+{
+	int h = strlen(s);
+
+	while (h--)
+		_putchar(*(s + h));
+	_putchar('\n');
+}
