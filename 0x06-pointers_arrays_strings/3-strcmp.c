e70923f8793d2509c8b8fa7910b6e18b66afb48e
Balogunmatt <balogunmatthew01@gmail.com>
2022-09-22 16:35:19 -0700

    compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,15 +16,20 @@ int _strcmp(char *s1, char *s2)
 	int i, diff;
 
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	if (s1 == s2)
+		diff = 0;
+	else
 	{
-		if (s1[i] != s2[i])
+		for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 		{
-			diff = s1[i] - s2[i];
-			break;
+			if (s1[i] != s2[i])
+			{
+				diff = s1[i] -s2[i];
+				break;
+			}
+			else
+				continue;
 		}
-		else
-			continue;
 	}
 	return (diff);
 }
