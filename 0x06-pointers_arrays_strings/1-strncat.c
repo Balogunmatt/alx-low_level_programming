3b42f5640813dca968c3ad4dbe5cb712ab9e02f8
Balogunmatt <balogunmatthew01@gmail.com>
2022-09-22 03:59:08 -0700

    concatenate strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,7 +17,7 @@ char *_strncat(char *dest, char *src, int n)
 	int len1 = strlen(dest);
 	int i;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 		dest[len1 + i] =  src[i];
 	return (dest);
 }
