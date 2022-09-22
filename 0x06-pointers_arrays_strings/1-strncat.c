aaa6aa7cf970e3dd8ddac8dddf499c0291c44c7a
Balogunmatt <balogunmatthew01@gmail.com>
2022-09-22 03:55:00 -0700

    concatenate string

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+
+/**
+ * _strncat - concatenate n characters
+ * @dest: destination string
+ * @src: source string
+ * @n: number of characters to be concatenated
+ *
+ * Return: @dest
+ */
+
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len1 = strlen(dest);
+	int i;
+
+	for (i = 0; i <= n; i++)
+		dest[len1 + i] =  src[i];
+	return (dest);
+}
