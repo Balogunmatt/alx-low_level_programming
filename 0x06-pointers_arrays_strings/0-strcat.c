228e2417e2e01f5cbd3a68f98a106c592e261bbe
Balogunmatt <balogunmatthew01@gmail.com>
2022-09-22 02:58:12 -0700

    concatenate strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,13 +15,18 @@ char *_strcat(char *dest, char *src)
 {
 	int len1, len2, i;
 
-	len1 = strlen(dest);
-	len2 = strlen(src);
+	len1 = 0;
+	len2 = 0;
+
+	while (dest[i] != '\0')
+		len1++;
+	while (src[i] != '\0')
+		len2++
 	
 	for (i = 0; i <= len2; i++)
 	{
 		dest[len1 + i] = src[i];
 	}
-	_putchar(10);
+	
 	return (dest);
 }
