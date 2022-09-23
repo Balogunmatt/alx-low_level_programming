c3c9db0a6aaa99f8dd8d0da5e08401cf488435b0
Balogunmatt <balogunmatthew01@gmail.com>
2022-09-22 17:24:22 -0700

    reverse an array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,10 +13,10 @@ void reverse_array(int *a, int n)
 {
 	int i;
 
-	for (i = n - 1; i > 0; i++)
+	for (i = n - 1; i > 0; i--)
 	{
 		printf("%d, ", a[i]);
-		if (a[0])
+		if (i == 0)
 			printf("%d\n", a[0]);
 	}
 }
