f793ac61bc6fdc9541ab557b0c19e7d4dc313bc8
Balogunmatt <balogunmatthew01@gmail.com>
2022-09-24 18:11:58 -0700

    reverse string

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -15,9 +15,10 @@ void reverse_array(int *a, int n)
 
 	for (i = 0; i < n/2; i++)
 	{
-		ch = *(a[i]);
-		*(a[i]) = *(a[n - 1 - i]);
-		*(a[n - 1 - i]) = ch;
+		ch = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = ch;
 	}
-	printf("%d", *a);
+	printf("%d", a);
+	printf("\n");
 }
