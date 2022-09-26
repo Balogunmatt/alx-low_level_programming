223d68f6ec740a8f513da6002db2d23c3d31f676
Balogunmatt <balogunmatthew01@gmail.com>
2022-09-26 16:21:29 -0700

    check for the first occurence of a given character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * _strchr - a function that returns the first occurence
+ * @s: string pointer
+ * @c: character to be checked for
+ *
+ * Return: pointer to the first occurence and NULL if not
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+}
