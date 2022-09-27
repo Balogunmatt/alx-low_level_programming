228c20a82f5c583342e319f04fef653ca25c3e08
Balogunmatt <balogunmatthew01@gmail.com>
2022-09-27 04:19:45 -0700

    find the first match

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+/**
+ * _strpbrk - returns the pointer to the first match
+ * @s: input string pointer
+ * @accept: set of character string for check
+ *
+ * Return: pointer to the first match or NULL if none
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (!accept[j])
+			return (0);
+		else
+			return (s[i]);
+	}
+}
