75e3cd20d46ec77305380bf016fc5aa87df7d8a5
Balogunmatt <balogunmatthew01@gmail.com>
2022-09-27 03:45:25 -0700

    find the span of character set

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+
+/**
+ * _strspn - returns the span of characters found
+ * @s: inputed string
+ * @accept: string to check
+ *
+ * Return: unsigned integer
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+	unsigned int len = 0
+
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (!accept[j])
+			break;
+		else
+			len++;
+	}
+	return (len);
+}
