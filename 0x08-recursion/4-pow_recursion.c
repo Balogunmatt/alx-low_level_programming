684dc1cb367b98bc11bb63e7274a6aca3d2baa60
Balogunmatt <balogunmatthew01@gmail.com>
2022-09-29 02:03:00 -0700

    find the exponenetial of a value

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - raise an integer to an index value
+ * @x: base value
+ * @y: index number
+ *
+ * Return: value of the base value raise to y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	else if (y < 0)
+		return (-1);
+	else
+		return (x * _pow_recursion(x, y - 1);
+}
