b5563d0eeaff1ef25461561c6156c26444b6f6b7
Balogunmatt <balogunmatthew01@gmail.com>
2022-10-02 14:15:10 -0700

    print the program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,12 +3,12 @@
 /**
  * main - entry point
  * @argc: number of arguments
- * @argv[]: array of string arguments
+ * @argv: array of string arguments
  *
  * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-	printf("./%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
